c2aa3082b5aca87995fbffcea92fa94bc22622a6
shin604 <148770999+shin604@users.noreply.github.com>
2023-11-12 08:04:30 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+const start = Date.now();
+
+const today = new Date("August 22, 2023");
+
+const year = today.getFullYear();
+
+const month = today.getMonth()+1;
+
+const date = today.getDate();
+
+console.log(year + '年' + month + '月' + date + '日');
